ba6f9be8599f7f22feece96a805b4401f95b01c5
Benjamin Chang <benjamin.30805@yahoo.com.tw>
2018-11-05 13:13:25 +0800

    把還不會程式時抄的程式碼修了一下，果然當時什麼都不懂

---
+++ calendar.cpp
@@ -0,0 +1,59 @@
+#include<stdio.h>
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int i,m,n,q;
+    int year, month, first_space, feb_days;
+   
+    cout << "請輸入西元";
+    cin >> year;
+      
+    cout << "請輸入月份" ;
+    cin >> month;
+    cout << endl;
+   
+   
+    if(month<=12 && month>=1){
+        for(first_space=5,q=0;q<(year-1700);q++){
+            if(q%4==0 && q%100!=0 || q%400==100)
+                first_space=(first_space+366)%7;
+            else
+                first_space=(first_space+365)%7;
+        }
+
+        //計算2月天數
+        if(year%4==0 && year%100!=0 || year%400==0)
+            feb_days =29;
+        else
+            feb_days =28;
+   
+    
+        int space[13]={0,first_space,0,0,0,0,0,0,0,0,0,0,0};  
+        int days[13]={0,31,feb_days,31,30,31,30,31,31,30,31,30,31};
+   
+
+
+        for(i=1;i<month;i++)
+            space[i+1]=(space[i]+days[i])%7;
+   
+   
+        cout << "       " << year << "年" << month << "月" << endl;
+        cout << " 日 一 二 三 四 五 六" << endl;
+         
+        for(m=1;m<=space[i];m++)
+            cout << "  ";
+             
+        for(n=1;n<=days[i];n++){
+            if(n<=9)
+                cout<< "  " << n;
+            else  
+                cout << " " << n;
+         
+            if(n%7==(7-space[i])%7)
+                cout << endl;
+        }
+        cout << endl;
+    }
+}
